f3bcf98941245880133db3032f3d6da4f9f2b955
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2025-09-09 12:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (24.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+ class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if (!isMatching(top, c)) return false;
+            }
+        }
+        return st.empty();
+    }
+
+private:
+    bool isMatching(char opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+               (opening == '{' && closing == '}') ||
+               (opening == '[' && closing == ']');
+    }
+};   
+    
