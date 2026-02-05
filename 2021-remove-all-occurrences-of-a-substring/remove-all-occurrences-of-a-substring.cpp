c2ce7209ea8dc116553aebb142a474a484d6a893
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-02-05 21:33:57 +0530

    Time: 2 ms (40.72%) | Memory: 9.3 MB (91.53%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -3,7 +3,6 @@ public:
     string removeOccurrences(string s, string part) {
         while(s.length()>0 && s.find(part)<s.length()){
             s.erase(s.find(part),part.length());
-            
         }
         return s;
     }
