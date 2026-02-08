0d3cf980e64be96144a43884d8c4f0c56bfd5855
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-02-08 10:44:52 +0530

    Time: 60 ms (27.35%) | Memory: 18.7 MB (66.88%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//     int row=matrix.size();
+//     int col=matrix[0].size();
+//     int rowindex=0;
+//     int colindex=col-1;
+//     while(rowindex< row && colindex>=0 )   {
+//         int element =matrix[rowindex][colindex];
+//         if(element==target){
+//             return 1;
+//         }
+//         if(element<target){
+//             rowindex++;
+//         }
+//         else{
+//             colindex--;
+//         }
+//     }
+//     return 0; 
+//     }
+// };
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int row=matrix.size();
-    int col=matrix[0].size();
-    int rowindex=0;
-    int colindex=col-1;
-    while(rowindex< row && colindex>=0 )   {
-        int element =matrix[rowindex][colindex];
-        if(element==target){
-            return 1;
-        }
-        if(element<target){
-            rowindex++;
-        }
-        else{
-            colindex--;
-        }
-    }
-    return 0; 
+        int n = matrix.size();
+        int m = matrix[0].size();
+          for(int i = 0;i < n ; i++){
+            if(target < matrix[i][0] || target > matrix[i][m-1]) 
+            continue;
+
+            int l = 0 , r = m-1;
+            while(l <= r){
+                int mid = (l+r)/2;
+                if(matrix[i][mid] == target){
+                    return true;
+                }
+                else if(matrix[i][mid] > target) r = mid-1;
+                else l = mid+1;
+            }
+          }
+             return false;
     }
 };
