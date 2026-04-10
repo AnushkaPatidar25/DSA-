90875f81d6a0435b682822e0d2c823d92003addb
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-04-10 10:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (98.91%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,30 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//        unordered_map<int,int>temp;
+//        for(int i=0;i<nums.size();i++){
+//           temp[nums[i]]++;
+//        }
+//        for(auto i:temp){
+//         if(i.second>nums.size()/2) {
+//             return i.first;
+//        }
+//     }
+//     return -1;
+//     }
+// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>temp;
-       for(int i=0;i<nums.size();i++){
-          temp[nums[i]]++;
-       }
-       for(auto i:temp){
-        if(i.second>nums.size()/2) {
-            return i.first;
-       }
-    }
-    return -1;
+      int candidate =0;
+      int count=0;
+      for(int i=0;i<nums.size();i++){
+        if(count==0){
+            candidate=nums[i];
+        }
+        if(candidate==nums[i])count++;
+        else count--;
+      }  
+      return candidate;
     }
 };
