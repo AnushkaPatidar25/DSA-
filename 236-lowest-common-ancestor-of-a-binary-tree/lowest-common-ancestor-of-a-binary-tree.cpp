ebfaff9218d95dd1675a37e442b518ca7f5f9246
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-06-15 14:53:11 +0530

    Time: 70 ms (6.94%) | Memory: 51.1 MB (5.04%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,19 +7,32 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
 class Solution {
 public:
+ bool solve(TreeNode* root,vector<TreeNode*>&path,TreeNode* target){
+    if(root==NULL) return false ;
+      path.push_back(root);
+      if(root==target) return true;
+    if(solve(root->left,path,target)||
+    solve(root->right,path,target)) return true;
+    path.pop_back();
+    return false;
+ 
+}
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q)
-            return root;
-        
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        
-        if (left && right)
-            return root;
-        
-        return left ? left : right;
+     vector<TreeNode*>path1;
+     vector<TreeNode*>path2;
+     solve(root,path1,p);
+     solve(root,path2,q);
+      int i=path1.size()-1;
+      int j=path2.size()-1;
+      while(i>=0&& j>=0){
+        if(path1[i]==path2[j])
+         return path1[i];
+        if(i>j) i--;
+        else j--;
+      }
+      return NULL;
     }
 };
