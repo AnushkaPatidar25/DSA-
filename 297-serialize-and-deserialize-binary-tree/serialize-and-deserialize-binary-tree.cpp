1eb61805f674ef640ed619c5cde30c04cce1f9fd
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-06-22 13:40:24 +0530

    Time: 25 ms (70.74%) | Memory: 36.2 MB (33.81%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -12,24 +12,26 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-      string st="";
-      if(root==NULL) return "";
-      queue<TreeNode*>q;
-      q.push(root);
-      while(!q.empty()){
-        TreeNode* front=q.front();
-        q.pop();
-        if(front==NULL){
-            st+="null,";
-            continue;
+      string s="";
+      if(root==NULL) return s;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+            TreeNode* front=q.front();
+            q.pop();
+            if(front==NULL){
+                s+="null,";
+                continue;
+            }
+             q.push(front->left);
+             q.push(front->right);
+             s+=to_string(front->val);
+             s+=",";
+             }
         }
- 
-        st+=to_string(front->val);
-        st+=",";
-        q.push(front->left);
-        q.push(front->right);
-      } 
-      return st;
+        return s;
     }
 
     // Decodes your encoded data to tree.
@@ -47,25 +49,24 @@ public:
             }
         }
         token.push_back(temp);
-        TreeNode* root=new TreeNode(stoi(token[0]));
+        TreeNode* root= new TreeNode(stoi(token[0]));
         queue<TreeNode*>q;
         q.push(root);
         int i=1;
-        while(!q.empty() && i<token.size()){
-            TreeNode* front=q.front();
-            q.pop();
-            if(token[i]!="null"){
-             front->left=new TreeNode(stoi(token[i]));
-             q.push(front->left);
-            }
-            i++;
-            if(i<token.size()&&token[i]!="null"){
-                front->right=new TreeNode(stoi(token[i]));
-                q.push(front->right);           
-          }
-          i++;
+        while(!q.empty()&&i<token.size()){
+         TreeNode* front=q.front();
+         q.pop();
+         if(token[i]!="null"){
+            front->left=new TreeNode(stoi(token[i]));
+            q.push(front->left);
+         }
+         i++;
+         if(token[i]!="null"&& i<token.size()){
+            front->right=new TreeNode(stoi(token[i]));
+            q.push(front->right);
+         }
+         i++;
         }
-       
         return root;
     }
 };
