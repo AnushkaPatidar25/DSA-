90c8ee8b32a7a72b8b460e66ad2e5561c8431555
AnushkaPatidar25 <anushkapatidar189@gmail.com>
2026-06-23 11:02:38 +0530

    Time: 10 ms (8.51%) | Memory: 19.3 MB (5.21%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     string frequencySort(string s) {
-     unordered_map<char,int> mp;
-     for(char ch:s){
+      map<char,int>mp;
+      for(char ch:s){
         mp[ch]++;
-     }
-     vector<string> buskect(s.length()+1);
-     for(auto it: mp){
-        char ch=it.first;
-        int freq=it.second;
-        buskect[freq]+=string(freq,ch);
-     }
-     string ans="";
-     for(int i=s.length();i>=0;i--){
-        ans+=buskect[i];
-     }
-     return ans;
+      } 
+      vector<string>bucket(s.length()+1);
+        for(auto a:mp){
+            char ch=a.first;
+            int freq=a.second;
+            bucket[freq]+=string(freq,ch);
+        }
+        string ans="";
+        for(int i=s.length();i>=0;i--){
+          ans+=bucket[i];
+        }
+      return ans;
     }
 };
